2f053ebe057dc96dd35b121e4d75f7917f744d15
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-02-11 15:21:00 +0700

    week02: add warning validation in init block

--- src/OOP_122030_ChristopherSunyoto_W02/PRAKTIKUM.kt
+++ src/OOP_122030_ChristopherSunyoto_W02/PRAKTIKUM.kt
@@ -1,6 +1,14 @@
 package oop_122030_ChristopherSunyoto.week02
 
-class Mahasiswa(val nama: String, val nim: String, val major: String)
+class Mahasiswa(val name: String, val nim: String, val major: String)
 {
-
+    init {
+        // Validasi sederhana: Cek Panjang NIM
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di memory.")
+        }
+    }
 }

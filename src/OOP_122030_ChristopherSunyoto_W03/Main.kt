260fc61a1aed85e2f124b9535fb091944dc44aab
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-02-18 15:48:54 +0700

    week03: cleanup code

--- src/OOP_122030_ChristopherSunyoto_W03/Main.kt
+++ src/OOP_122030_ChristopherSunyoto_W03/Main.kt
@@ -3,15 +3,12 @@ package oop_122030_ChristopherSunyoto.week03
 fun main() {
     val e = Employee("Budi")
 
-    // 1. Test Validasi Salary
-    e.salary = -1000 //  Harusnya print Error
+    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    //2. Test encapsulation
-    e.increasePerformance()
-    // e.performanceRating = 5 // coba uncomment, pasti merah (Error)
 
-    // 3. Test computed Property
+    e.increasePerformance()
+
     println("Pajak yang harus dibayar: ${e.tax}")
 }

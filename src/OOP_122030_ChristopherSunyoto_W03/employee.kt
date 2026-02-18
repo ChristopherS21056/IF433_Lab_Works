076d3861c43d267d70bccd4805f202e2c21adf12
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-02-18 15:23:28 +0700

    week 03: simulate stack overflow in setter

--- src/OOP_122030_ChristopherSunyoto_W03/employee.kt
+++ src/OOP_122030_ChristopherSunyoto_W03/employee.kt
@@ -1,5 +1,12 @@
 package oop_122030_ChristopherSunyoto.W03
 
 class Employee(val name: String) {
-    val salary: Int = 0
+    var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            // PERINGATAN: KODE DI BAWAH INI SALAH DAN BERBAHAYA!
+            // kITA MEMANGGIL SETTER DI DALAM SETTER (recursive)
+           this.salary = value
+
+        }
 }

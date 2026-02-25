2edde922bcd3d7d587ec3788687a345498805cef
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-02-25 15:37:00 +0700

    week04: (task 2) test employee hierarchy in main

--- src/oop_122030_ChristopherSunyoto_Week04/tugas2/Main.kt
+++ src/oop_122030_ChristopherSunyoto_Week04/tugas2/Main.kt
@@ -1,5 +1,13 @@
 package oop_122030_ChristopherSunyoto_Week04.tugas2
 
 fun main() {
-    p
+    val manager = Manager("Christopher", 8000000)
+    val developer = Developer("Vassel", 7000000, "Kotlin")
+    manager.work()
+
+    println("Bonus Manager: ${manager.calculateBonus()}")
+    println()
+
+    developer.work()
+    println("Bonus Developer: ${developer.calculateBonus()}")
 }

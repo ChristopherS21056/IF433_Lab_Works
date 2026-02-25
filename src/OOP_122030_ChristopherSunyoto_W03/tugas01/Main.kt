be49f97fca16b1a0b8b0f501968f18e12107c986
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-02-25 14:42:59 +0700

    weeko4: create basic vehicle parent class

--- src/OOP_122030_ChristopherSunyoto_W03/tugas01/Main.kt
+++ src/OOP_122030_ChristopherSunyoto_W03/tugas01/Main.kt
@@ -1,11 +1,11 @@
-package oop_122030_ChristopherSunyoto.week03.tugas01
-
-fun main() {
-    val weapon = Weapon("Dragon Slayer")
-    weapon.damage = -50
-    weapon.damage = 9999
-
-    println("Nama Weapon : ${weapon.name}")
-    println("Damage      : ${weapon.damage}")
-    println("Tier        : ${weapon.tier}")
-}
+//package oop_122030_ChristopherSunyoto.week03.tugas01
+//
+//fun main() {
+//    val weapon = Weapon("Dragon Slayer")
+//    weapon.damage = -50
+//    weapon.damage = 9999
+//
+//    println("Nama Weapon : ${weapon.name}")
+//    println("Damage      : ${weapon.damage}")
+//    println("Tier        : ${weapon.tier}")
+//}
---
+++ src/oop_122030_ChristopherSunyoto_Week04/Pasta.kt
@@ -0,0 +1,13 @@
+package oop_122030_ChristopherSunyoto_Week04
+
+class Pasta:Makanan() {
+    private var toping:String=""
+    public fun pasta_terjual(jml:Int) {
+        this.toping="nora"
+        super.harga = jml * 120
+        println("Pasta $toping laris $harga")
+    }
+    override public fun jual_makanan(){
+        println("Saya jual PASTA yang enak")
+    }
+}

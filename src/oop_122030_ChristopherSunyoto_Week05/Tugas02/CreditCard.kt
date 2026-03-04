d62d7308ea449a239dc5383cca4383d082b6c3ff
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-04 14:56:33 +0700

    week05 : (task 2) create CreditCard class

--- src/oop_122030_ChristopherSunyoto_Week05/Tugas02/CreditCard.kt
+++ src/oop_122030_ChristopherSunyoto_Week05/Tugas02/CreditCard.kt
@@ -1,2 +1,14 @@
 package oop_122030_ChristopherSunyoto_Week05
 
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("[$accountName] Sukses! Pembayaran kartu kredit berhasil. Limit terpakai: $usedAmount / $limit")
+        } else {
+            println("[$accountName] Gagal: Transaksi ditolak karena melebihi limit.")
+        }
+    }
+}

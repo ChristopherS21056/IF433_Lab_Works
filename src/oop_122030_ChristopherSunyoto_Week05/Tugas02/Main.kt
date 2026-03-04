0ecafe3c5f794731187db15af6d82b5aad35744c
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-04 14:58:45 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_122030_ChristopherSunyoto_Week05/Tugas02/Main.kt
@@ -0,0 +1,23 @@
+package oop_122030_ChristopherSunyoto_Week05
+
+fun main() {
+    println("\n--- TUGAS 2: SISTEM PEMBAYARAN ---")
+
+    val myEWallet = EWallet("Gopay Budi", 50000.0)
+    val myCreditCard = CreditCard("BCA Budi", 100000.0)
+
+    val paymentList: List<PaymentMethod> = listOf(myEWallet, myCreditCard)
+
+    for (payment in paymentList) {
+        println("\nMemproses tagihan 75000.0 untuk: ${payment.accountName}")
+        payment.processPayment(75000.0)
+
+        if (payment is EWallet) {
+           println(">> Mendeteksi e-wallet dengan saldo kurang. Melakukan auto Top-Up...")
+            payment.topUp(50000.0)
+
+            println(">> Mencoba proses pembayaran kembali...")
+            payment.processPayment(75000.0)
+        }
+    }
+}

8b5cffc9f42291b14b45392f2015f930abc72fdd
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-11 15:23:50 +0700

    week06 : Create Compound Interfaces

---
+++ src/oop_122030_ChristopherSunyoto_Week06/lab/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_122030_ChristopherSunyoto_Week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}

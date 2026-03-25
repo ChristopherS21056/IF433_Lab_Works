464943aeff3d47da80aec7689d981e5e9c2e7220
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-25 10:45:20 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/Main.kt
+++ src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/Main.kt
@@ -11,4 +11,14 @@ fun main() {
     val playerWeapon = Weapon.forgeStarterSword()
     println("Pemain mendapatkan senjata: ${playerWeapon.item.name}")
     println("Detail -> Damage: ${playerWeapon.item.damage}, Rarity: ${playerWeapon.item.rarity}, Durability: ${playerWeapon.durability}")
+
+    println("\n--- Simulasi Perjalanan Pemain ---")
+    val upgradedSword = playerWeapon.item.copy(
+        name = "Pedang Kayu Legendaris",
+        damage = 25
+    )
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedSword))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }

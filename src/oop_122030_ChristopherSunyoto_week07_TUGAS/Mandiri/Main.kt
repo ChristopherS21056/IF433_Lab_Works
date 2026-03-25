47250955382f4825030de81bd9dc763b2b5bdcec
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-25 10:39:50 +0700

    week07: (task) implement exhaustive processEvent

--- src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/Main.kt
+++
@@ -1,8 +0,0 @@
-package oop_122030_ChristopherSunyoto_week07_TUGAS.Mandiri
-
-fun main() {
-    println("--- Memulai Simulasi RPG ---")
-
-    GameManager.startGame()
-    GameManager.startGame()
-}

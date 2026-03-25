60c6e83db0fee1924aa9e84d84d9a187a2b70324
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-25 10:39:08 +0700

    week07: (task) implement exhaustive processEvent

--- src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/GameExecutor.kt
+++ src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/GameExecutor.kt
@@ -1,8 +1,19 @@
 package oop_122030_ChristopherSunyoto_week07_TUGAS.Mandiri
 
-sealed class BattleState {
-    data class MonsterEncounter(val monsterName: String) : BattleState()
-    data class LootDropped(val item: GameItem) : BattleState()
-    data class GameOver(val reason: String) : BattleState()
-    object SafeZone : BattleState()
+fun processEvent(event: BattleState) {
+    when (event) {
+        is BattleState.MonsterEncounter -> {
+            println("Waspada! ${event.monsterName} muncul dari semak-semak!")
+        }
+        is BattleState.LootDropped -> {
+            val item = event.item
+            println("Harta Karun! Anda mendapatkan ${item.name} [Rarity: ${item.rarity}]")
+        }
+        is BattleState.GameOver -> {
+            println("Permainan Berakhir: ${event.reason}")
+        }
+        BattleState.SafeZone -> {
+            println("Anda memasuki Zona Aman. Tidak ada musuh di sini.")
+        }
+    }
 }

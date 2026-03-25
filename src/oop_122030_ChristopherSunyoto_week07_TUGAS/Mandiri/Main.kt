d93d445680778984bc9f581c0deac5f012380699
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-25 10:41:05 +0700

    week07: (task) test GameManager singleton in main

---
+++ src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/Main.kt
@@ -0,0 +1,8 @@
+package oop_122030_ChristopherSunyoto_week07_TUGAS.Mandiri
+
+fun main() {
+    println("--- Memulai Simulasi RPG ---")
+
+    GameManager.startGame()
+    GameManager.startGame()
+}

dd9c81224509584e613a762c7993dd741d553971
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-25 08:37:43 +0700

    week07: test singleton and companion object in main

--- src/oop_122030_ChristopherSunyoto_week07_TUGAS/Main.kt
+++ src/oop_122030_ChristopherSunyoto_week07_TUGAS/Main.kt
@@ -1,3 +1,11 @@
 package oop_122030_ChristopherSunyoto_week07_TUGAS
 
-val client = NetworkClient("https://api.umn.ac.id")
+fun main () {
+    println("=== TEST SINGLETONB ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient() // Instansiasi lewat factory
+    client.connect()
+}

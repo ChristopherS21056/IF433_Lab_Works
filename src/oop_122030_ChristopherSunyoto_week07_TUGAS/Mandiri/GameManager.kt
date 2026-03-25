bda2068288ac91ba66f113859bf6985f2a725c95
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-25 09:09:06 +0700

    week07: (task) createe GameManager Singleton

---
+++ src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/GameManager.kt
@@ -0,0 +1,19 @@
+package oop_122030_ChristopherSunyoto_week07_TUGAS.Mandiri
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
+
+fun main() {
+    GameManager.startGame()
+    GameManager.startGame()
+}

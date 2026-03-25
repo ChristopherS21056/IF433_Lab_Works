cd67d556c5eb557e6d8c21041909abda0a2da85a
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-25 10:42:48 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/Main.kt
+++ src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/Main.kt
@@ -5,4 +5,10 @@ fun main() {
 
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\n--- Simulasi Factory & Enum ---")
+    println("Drop chance item LEGENDARY adalah: ${ItemRarity.LEGENDARY.dropChance}%")
+    val playerWeapon = Weapon.forgeStarterSword()
+    println("Pemain mendapatkan senjata: ${playerWeapon.item.name}")
+    println("Detail -> Damage: ${playerWeapon.item.damage}, Rarity: ${playerWeapon.item.rarity}, Durability: ${playerWeapon.durability}")
 }

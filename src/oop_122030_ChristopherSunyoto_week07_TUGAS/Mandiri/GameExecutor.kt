898c1712489450776e91375b48e5a94071feeb81
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-03-25 10:33:09 +0700

    week07: (task) implement exhaustive processEvent

---
+++ src/oop_122030_ChristopherSunyoto_week07_TUGAS/Mandiri/GameExecutor.kt
@@ -0,0 +1,8 @@
+package oop_122030_ChristopherSunyoto_week07_TUGAS.Mandiri
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState()
+}

ecae4012ac7138005055aec3d6ec345079829c51
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-15 15:34:46 +0700

    week08: create mixed data collection

--- src/oop_122030_ChristopherSunyoto_Week08/TUGAS/Main.kt
+++ src/oop_122030_ChristopherSunyoto_Week08/TUGAS/Main.kt
@@ -17,5 +17,14 @@ fun main() {
         "Transaksi Valid. Harga: nRp$price, Pajak: Rp$tax"
     } ?: "Transaksi Invalid: Harga belum di-set!"
 
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
+
     println(receipt)
 }

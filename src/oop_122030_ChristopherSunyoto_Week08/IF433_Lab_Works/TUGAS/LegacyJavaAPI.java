9866105061df84287d9820ca15ddfa85d6bf372a
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-15 15:52:31 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_122030_ChristopherSunyoto_Week08/IF433_Lab_Works/TUGAS/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_122030_ChristopherSunyoto_Week08.IF433_Lab_Works.TUGAS;
+
+public class LegacyJavaAPI {
+    // Fungsi Java yang tidak diketahui nullability-nya oleh Kotlin
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+}

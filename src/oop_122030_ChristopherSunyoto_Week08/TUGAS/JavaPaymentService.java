9abbafed2e649bcee13c7b373c13702f7d3f7c2e
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-15 16:08:08 +0700

    week08: (task) create Java Payment Service for payment interop

---
+++ src/oop_122030_ChristopherSunyoto_Week08/TUGAS/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_122030_ChristopherSunyoto_Week08.TUGAS
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}

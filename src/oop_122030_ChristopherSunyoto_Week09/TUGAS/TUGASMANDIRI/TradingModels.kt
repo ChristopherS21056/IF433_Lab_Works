07d07a73fe013927d0229bceb58e68fcadce46b5
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-22 14:30:16 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_122030_ChristopherSunyoto_Week09/TUGAS/TUGASMANDIRI/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_122030_ChristopherSunyoto_Week09.TUGAS.TUGASMANDIRI
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)

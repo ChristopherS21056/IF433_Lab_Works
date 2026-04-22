deda9942ab954b42fb2341bf661f25af168b07bf
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-22 14:48:03 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_122030_ChristopherSunyoto_Week09/TUGAS/TUGASMANDIRI/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_122030_ChristopherSunyoto_Week09.TUGAS.TUGASMANDIRI
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 10, 25.5, "CLOSED"), // Profit, CLOSED
+        TradeLog("ETHUSDT", "SHORT", 20, -15.0, "CLOSED"), // Loss, CLOSED
+        TradeLog("BTCUSDT", "LONG", 50, 120.5, "OPEN"),    // Profit, OPEN
+        TradeLog("SOLUSDT", "SHORT", 5, -8.2, "OPEN"),     // Loss, OPEN
+        TradeLog("ETHUSDT", "LONG", 10, 12.0, "CLOSED"),   // Profit, CLOSED
+        TradeLog("BTCUSDT", "SHORT", 25, -5.5, "CLOSED")   // Loss, CLOSED
+    )
+}

ff2b1a0ac783e473a29d9bc7093e5038cf7c685c
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-22 14:10:07 +0700

    week09: create Student data class and list

---
+++ src/oop_122030_ChristopherSunyoto_Week09/TUGAS/ObjectChaining.kt
@@ -0,0 +1,12 @@
+package oop_122030_ChristopherSunyoto_Week09.TUGAS
+
+data class Student(val name: String, val gpa: Double)
+
+fun main() {
+    val students = listOf(
+        Student("Andi", 3.2),
+        Student("Budi", 3.2),
+        Student("Citra", 3.2),
+        Student("Dewi", 3.2)
+    )
+}

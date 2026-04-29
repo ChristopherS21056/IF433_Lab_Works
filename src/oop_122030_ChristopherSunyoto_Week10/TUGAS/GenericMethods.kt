ee4535d80781202de3fecda7922c85598720ac3c
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-29 07:30:45 +0700

    week10: implement generic function with return type

--- src/oop_122030_ChristopherSunyoto_Week10/TUGAS/GenericMethods.kt
+++ src/oop_122030_ChristopherSunyoto_Week10/TUGAS/GenericMethods.kt
@@ -2,4 +2,10 @@ package oop_122030_ChristopherSunyoto_Week10.TUGAS
 
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
+
+    fun <T> processData(input: T): T {
+        return input
+    }
+
+
 }

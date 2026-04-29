1c1d10595ec081149cc76e6385ec65190ef0b681
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-29 07:38:02 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_122030_ChristopherSunyoto_Week10/TUGAS/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_122030_ChristopherSunyoto_Week10.TUGAS
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}

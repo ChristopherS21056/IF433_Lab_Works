33eb874d2a7c1612f6fc8a46e2bd671944cccad6
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-29 07:40:11 +0700

    week10: (task) implement search logic in repository

--- src/oop_122030_ChristopherSunyoto_Week10/TUGAS/NetworkResponse.kt
+++ src/oop_122030_ChristopherSunyoto_Week10/TUGAS/NetworkResponse.kt
@@ -1,3 +1,5 @@
 package oop_122030_ChristopherSunyoto_Week10.TUGAS
 
 data class ApiResponse<T>(val status: String, val data: T)
+
+
--- src/oop_122030_ChristopherSunyoto_Week10/TUGAS/WalletRepository.kt
+++ src/oop_122030_ChristopherSunyoto_Week10/TUGAS/WalletRepository.kt
@@ -1,6 +1,6 @@
 package oop_122030_ChristopherSunyoto_Week10.TUGAS
 
-class WalletRepository<T> {
+class WalletRepository<T : Any> { // Menggunakan constraint <T : Any>
     private val items = mutableListOf<T>()
 
     fun add(item: T) {
@@ -10,4 +10,9 @@ class WalletRepository<T> {
     fun getAll(): List<T> {
         return items
     }
+
+    fun findByName(query: String): List<T> {
+        return items.filter { it.toString().contains(query, ignoreCase = true) }
+    }
 }
+

b315a150f849560ece1e9a155038f0620e324d0a
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-04-29 07:41:36 +0700

    week10: (task) populate coin repository

--- src/oop_122030_ChristopherSunyoto_Week10/TUGAS/CryptoDashboard.kt
+++ src/oop_122030_ChristopherSunyoto_Week10/TUGAS/CryptoDashboard.kt
@@ -2,4 +2,11 @@ package oop_122030_ChristopherSunyoto_Week10.TUGAS
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 10.2))
+    coinRepo.add(Coin("USDT", 5000.0))
+
+    println("Isi Dompet: ${coinRepo.getAll()}")
+
 }

66e757ddad423581d243f74c3496fe59e787d65f
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-05-06 13:27:15 +0700

    week11: (task) implement device search with let

--- src/OOP_122030_ChristopherSunyoto_Week11/coba.kt
+++ src/OOP_122030_ChristopherSunyoto_Week11/coba.kt
@@ -1,2 +1,29 @@
-package oop_122030_ChristopherSunyoto_Week11.TUGAS
+package oop_122030_ChristopherSunyoto_Week11
 
+// function biasa
+fun panggil_nama (nama:String): String {
+    return "Hai $nama"
+}
+
+// function extensionfunction - basic
+fun String.panggil_mahasiswa(): String {
+    // return "Hello $this"
+    var hasil = this.split(" ").joinToString (" "){
+        it.replaceFirstChar {
+            c -> c.uppercase()
+        }
+    }
+
+    return hasil
+}
+
+fun main() {
+    // cara panggil function biasa
+    println(panggil_nama("Toni").uppercase())
+
+    // cara panggil function extension - BASIC
+    println("tonyy stark iron man".panggil_mahasiswa())
+
+
+}
+//

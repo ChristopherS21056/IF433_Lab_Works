d6336730a7d224e40cfd38a60736c688bb11e04b
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-05-06 12:43:18 +0700

    week11: test extension functions in main

---
+++ src/OOP_122030_ChristopherSunyoto_Week11/TUGAS/Main.kt
@@ -0,0 +1,10 @@
+package oop_122030_ChristopherSunyoto_Week11.TUGAS
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}

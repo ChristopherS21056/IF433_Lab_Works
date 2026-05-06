c3945cecf7a9b523271cd3b65276f701587c6b05
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-05-06 12:50:52 +0700

    week11: (task) create SmartDevice data class

---
+++ src/OOP_122030_ChristopherSunyoto_Week11/TUGAS/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_122030_ChristopherSunyoto_Week11.TUGAS
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)

029ab1c3087da0284f81176f0018ea63a0ded172
ChristopherS21056 <christophersunyoto82@gmail.com>
2026-05-13 02:33:30 +0700

    week12: (task) handle feeding success using onSuccess

--- src/OOP_122030_ChristopherSunyoto_WEEK12/TUGAS/SmartFeeder.kt
+++ src/OOP_122030_ChristopherSunyoto_WEEK12/TUGAS/SmartFeeder.kt
@@ -35,5 +35,8 @@ fun main() {
 
     runCatching {
         dispenseKibble(30, currentKibbleStock, false)
+    } .onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
     }
 }
